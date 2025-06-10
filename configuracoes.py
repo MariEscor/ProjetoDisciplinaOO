7bf3bda1ec07d3b4808cf53d025254b2abe373ef
Mariana Escorcer <110674324+MariEscor@users.noreply.github.com>
2025-06-09 23:56:18 -0300

    Add files via upload

---
+++ configuracoes.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.math import Vector2 as vector
+from sys import exit
+
+janela_largura, janela_altura = 1280, 720
+tile_size = 64
+velocidade_animacao = 6
+battle_outline_width = 4
+
+colors = {
+
+}
+
+world_layers = {
+
+}
+
+battle_positions = {
+
+}
+
+battle_layers = {
+
+}
+
+battle_choices = {
+    
+}
