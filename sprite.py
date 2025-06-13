f2edb1e56f42f175662469980dcb09a95092cbb9
Rafael Verissimo <123127434+Piucles@users.noreply.github.com>
2025-06-13 01:11:56 -0300

    Delete sprite.py

--- sprite.py
+++
@@ -1,8 +0,0 @@
-from config import Config
-import pygame
-
-class Sprite(pygame.sprite.Sprite):
-    def __init__(self, posicao, superficie, grupos):
-        super().__init__(grupos)
-        self.image = superficie
-        self.rect = self.image.get_rect(topleft = posicao)
